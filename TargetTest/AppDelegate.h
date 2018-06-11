b3d50d4cf9869fc8826635034c151042cac4e20e
mac <mac@mac-mini.local>
2018-06-11 11:34:31 +0800

    Initial Commit

---
+++ TargetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TargetTest
+//
+//  Created by mac on 2018/6/11.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TargetTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TargetTest
+//
+//  Created by mac on 2018/6/11.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
